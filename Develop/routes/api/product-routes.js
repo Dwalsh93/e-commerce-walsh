a05230a40f16c61ae07fc881e4a30cb2c8c9dd27
dillon walsh <dillonwalsh93@gmail.com>
2021-07-17 13:39:41 -0400

    api product  routes updated

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,12 +6,46 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
-});
+  product.findAll({ //select statement 
+  
 
+    include: [
+      Category, {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  }).then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    res.status(500).json(err);
+  });
+  // be sure to include its associated Category and Tag data
+})
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category, {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({message: 'id could not be matched to a product' });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Category and Tag data
 });
 
@@ -91,6 +125,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No Product found! '});
+      return;
+    }
+    res.json(dbProductData);
+  }) 
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
